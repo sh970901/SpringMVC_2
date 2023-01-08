792da19eb222db81d5cac949ad99d96bcff6b156
sh970901 <aaddss639@naver.com>
2023-01-08 19:50:40 +0900

    에러코드, 오브젝트 오류, 수정 적용

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,15 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//@ScriptAssert(lang="javascript", script = "_this.price * _this.quantity >= 10000", message = "총합이 10000원 넘게 입력부탁드립니다.")
+//컨트롤러에서 자바코드로 처리하는 것도 괜찮은 방법 (Object Error의 경우)
 public class Item {
 
     private Long id;
