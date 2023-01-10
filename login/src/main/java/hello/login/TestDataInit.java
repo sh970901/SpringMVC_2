a5b51a165233f8f8f52946bd1a0de7a42baf17f2
sh970901 <aaddss639@naver.com>
2023-01-10 20:24:15 +0900

    회원가입 기능

--- login/src/main/java/hello/login/TestDataInit.java
+++ login/src/main/java/hello/login/TestDataInit.java
@@ -2,6 +2,8 @@ package hello.login;
 
 import hello.login.domain.item.Item;
 import hello.login.domain.item.ItemRepository;
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,7 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class TestDataInit {
 
+    private final MemberRepository memberRepository;
     private final ItemRepository itemRepository;
 
     /**
@@ -20,6 +23,12 @@ public class TestDataInit {
     public void init() {
         itemRepository.save(new Item("itemA", 10000, 10));
         itemRepository.save(new Item("itemB", 20000, 20));
+
+        Member member = new Member();
+        member.setLoginId("test");
+        member.setPassword("test!");
+        member.setName("테스터");
+        memberRepository.save(member);
     }
 
 }
