9a3fbde951204ca413ecf573cab2db9db8f63c9c
sh970901 <aaddss639@naver.com>
2023-01-14 17:05:20 +0900

    Exception Resolver

--- exception/src/main/java/hello/exception/servlet/ErrorPageController.java
+++ exception/src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,10 +1,18 @@
 package hello.exception.servlet;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -25,12 +33,29 @@ public class ErrorPageController {
         return "error-page/404";
     }
     @RequestMapping("/error-page/500")
-    public String errorPage500(HttpServletRequest request, HttpServletResponse
-            response) {
+    public String errorPage500(HttpServletRequest request, HttpServletResponse response) {
         log.info("errorPage 500");
         return "error-page/500";
     }
+
+    //같은 url도 타입으로 나눌 수 있음
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(HttpServletRequest
+                                                                       request, HttpServletResponse response) {
+        log.info("API errorPage 500");
+        System.out.println("request는" + request);
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+        Integer statusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        return new ResponseEntity(result, HttpStatus.valueOf(statusCode));
+    }
+
+
     private void printErrorInfo(HttpServletRequest request) {
+
         log.info("ERROR_EXCEPTION: ex=",
                 request.getAttribute(ERROR_EXCEPTION));
         log.info("ERROR_EXCEPTION_TYPE: {}",
